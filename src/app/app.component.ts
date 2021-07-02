a6f8c37db4c141e6d0eab1f7baf6711b7b33b03e
jorge palacios <reynald0010794@gmail.com>
2021-07-02 11:49:13 -0500

    make it better

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'practica2';
+  public autores = "Palacios - Joffre - Ronny";
+  public mostrar_lista = true;
+  public lista_de_usuarios = [ 'Palacios', 'Asencio', 'Espinoza', 'Vega', 'Marin'];
+  public texto_boton = "Ocultar lista";  
+  change():void{
+    this.mostrar_lista = !this.mostrar_lista;
+
+    if(this.mostrar_lista == false){
+      this.texto_boton = "Mostrar lista";
+    }else{
+      this.texto_boton = "Ocultar lista";
+    }
+  }
+  buscar() { 
+    var hay_coincidencias = false;
+    for(var i=0; i<this.lista_de_usuarios.length; i++){
+      if(this.lista_de_usuarios[i] == this.autores){
+        hay_coincidencias = true;
+      }
+    }
+    if(hay_coincidencias==true){
+      alert('Si hay coincidencias con la búsqueda');
+    }else{
+      alert('No hay coincidencias con la búsqueda');
+    }
+  }
+
+}
